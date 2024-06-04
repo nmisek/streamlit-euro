c076fe3be290de63167415ef172ba9a9c077a796
Nicole Misek <nicole@nextmv.io>
2024-06-04 16:53:40 -0400

    ..

--- input_set_create_view.py
+++ input_set_create_view.py
@@ -233,8 +233,6 @@ with st.form(key="worker_availability_form"):
             )
         else:
             # Overwrite existing availabilities with the new one
-            st.write(workers.loc[workers["id"] == worker_id, "availability"])
-            st.write(new_availability)
             workers.loc[workers["id"] == worker_id, "availability"] = workers.loc[
                 workers["id"] == worker_id, "availability"
             ].apply(lambda x: [new_availability])
